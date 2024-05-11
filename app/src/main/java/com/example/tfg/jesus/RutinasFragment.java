bfe3472d98566b2340852adf15c6f57c41fd3d9a
JCSM17 <jcsanchezmoreno7@gmail.com>
2024-05-12 01:41:40 +0200

    12/05 Animaciones y responsive

--- app/src/main/java/com/example/tfg/jesus/RutinasFragment.java
+++ app/src/main/java/com/example/tfg/jesus/RutinasFragment.java
@@ -3,50 +3,40 @@ package com.example.tfg.jesus;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.tfg.R;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RutinasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RutinasFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // Constantes para los nombres de los argumentos que este fragmento puede aceptar
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+    // Variables de instancia para almacenar los valores de los argumentos
     private String mParam1;
     private String mParam2;
 
+    // Constructor vacío requerido para instanciar el fragmento
     public RutinasFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RutinasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    // Método de fábrica estático para crear una nueva instancia de RutinasFragment con los argumentos proporcionados
     public static RutinasFragment newInstance(String param1, String param2) {
         RutinasFragment fragment = new RutinasFragment();
         Bundle args = new Bundle();
@@ -56,6 +46,7 @@ public class RutinasFragment extends Fragment {
         return fragment;
     }
 
+    // Este método se llama cuando se crea el fragmento. Aquí se recuperan los argumentos pasados al fragmento y se almacenan en las variables de instancia
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,19 +56,19 @@ public class RutinasFragment extends Fragment {
         }
     }
 
-
+    // Este método se llama para inflar la vista del fragmento. Aquí se infla el layout fragment_volumen_jueves
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rutina_volumen, container, false);
+        return inflater.inflate(R.layout.fragment_volumen_jueves, container, false);
     }
 
+    // Este método se llama después de que la vista del fragmento se ha creado. Aquí se configuran los listeners de los botones y la animación de la ImageView
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Map of button IDs to YouTube URLs
+        // Crear un mapa de IDs de botones a URLs de YouTube
         Map<Integer, String> buttonToUrlMap = new HashMap<>();
         buttonToUrlMap.put(R.id.botonVideoPressBanca, "https://www.youtube.com/shorts/i14IBMNQDQQ");
         buttonToUrlMap.put(R.id.botonVideoFondos, "https://www.youtube.com/shorts/lC7lLkjDZ_k");
@@ -85,15 +76,43 @@ public class RutinasFragment extends Fragment {
         buttonToUrlMap.put(R.id.buttonFrancesMancuerna, "https://www.youtube.com/shorts/DFFD1LU_iXw");
         buttonToUrlMap.put(R.id.botonVideoExtTriceps, "https://www.youtube.com/shorts/JVc1KAB_HLY");
         buttonToUrlMap.put(R.id.botonVideoPressPalof, "https://www.youtube.com/shorts/r3fM413P3W0");
-        // Add more button IDs and URLs as needed
 
-        // Set up click listeners for each button
+        // Configurar los listeners de los botones para abrir los videos de YouTube correspondientes cuando se hace clic en ellos
         for (Map.Entry<Integer, String> entry : buttonToUrlMap.entrySet()) {
             Button button = view.findViewById(entry.getKey());
             button.setOnClickListener(v -> openYoutubeVideo(entry.getValue()));
         }
+
+        // Array de lista de IDs de las ImageView a las que quieres aplicar la animación
+        int[] imageViewIds = {
+                R.id.imagenPBP,
+                R.id.imagenFondos,
+                R.id.imagenCruces,
+                R.id.imagePressFrances,
+                R.id.imagenExtTriceps,
+                R.id.imagenPalof
+        };
+        // Configurar un listener para cada ImageView. Cuando se hace clic en una ImageView, se inicia una animación de rotación que dura 3 segundos. Después de 3 segundos, la animación se detiene
+        for (int id : imageViewIds) {
+            ImageView imageView = view.findViewById(id);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate);
+                    imageView.startAnimation(animation);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.clearAnimation();
+                        }
+                    }, 3000);
+                }
+            });
+        }
     }
 
+    // Este método se utiliza para abrir un video de YouTube en el navegador. Se crea un Intent con la acción Intent.ACTION_VIEW y la URL del video de YouTube, y luego se inicia este Intent
     private void openYoutubeVideo(String url) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(url));
