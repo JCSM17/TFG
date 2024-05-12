ee8dcba0ae84c6d317ec18677ddedf32db883836
JCSM17 <jcsanchezmoreno7@gmail.com>
2024-05-12 17:21:46 +0200

    12/05 Cuenta Atrás

--- app/src/main/java/com/example/tfg/jesus/RutinasFragment.java
+++ app/src/main/java/com/example/tfg/jesus/RutinasFragment.java
@@ -1,8 +1,14 @@
 package com.example.tfg.jesus;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +17,7 @@ import android.widget.ImageView;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,6 +37,16 @@ public class RutinasFragment extends Fragment {
     // Variables de instancia para almacenar los valores de los argumentos
     private String mParam1;
     private String mParam2;
+
+    // URLs de los videos de YouTube
+    private static final Map<Integer, String> BUTTON_TO_URL_MAP = new HashMap<Integer, String>() {{
+        put(R.id.botonVideoPressBanca, "https://www.youtube.com/shorts/i14IBMNQDQQ");
+        put(R.id.botonVideoFondos, "https://www.youtube.com/shorts/lC7lLkjDZ_k");
+        put(R.id.botonVideoPolea, "https://www.youtube.com/shorts/wHKYSABOpGY");
+        put(R.id.buttonFrancesMancuerna, "https://www.youtube.com/shorts/DFFD1LU_iXw");
+        put(R.id.botonVideoExtTriceps, "https://www.youtube.com/shorts/JVc1KAB_HLY");
+        put(R.id.botonVideoPressPalof, "https://www.youtube.com/shorts/r3fM413P3W0");
+    }};
 
     // Constructor vacío requerido para instanciar el fragmento
     public RutinasFragment() {
@@ -68,22 +85,21 @@ public class RutinasFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Crear un mapa de IDs de botones a URLs de YouTube
-        Map<Integer, String> buttonToUrlMap = new HashMap<>();
-        buttonToUrlMap.put(R.id.botonVideoPressBanca, "https://www.youtube.com/shorts/i14IBMNQDQQ");
-        buttonToUrlMap.put(R.id.botonVideoFondos, "https://www.youtube.com/shorts/lC7lLkjDZ_k");
-        buttonToUrlMap.put(R.id.botonVideoPolea, "https://www.youtube.com/shorts/wHKYSABOpGY");
-        buttonToUrlMap.put(R.id.buttonFrancesMancuerna, "https://www.youtube.com/shorts/DFFD1LU_iXw");
-        buttonToUrlMap.put(R.id.botonVideoExtTriceps, "https://www.youtube.com/shorts/JVc1KAB_HLY");
-        buttonToUrlMap.put(R.id.botonVideoPressPalof, "https://www.youtube.com/shorts/r3fM413P3W0");
+        setupButtons(view);
+        setupImageViewAnimations(view);
+        setupCountDownTimer(view);
+    }
 
-        // Configurar los listeners de los botones para abrir los videos de YouTube correspondientes cuando se hace clic en ellos
-        for (Map.Entry<Integer, String> entry : buttonToUrlMap.entrySet()) {
+    // Configura los listeners de los botones para abrir los videos de YouTube correspondientes cuando se hace clic en ellos
+    private void setupButtons(View view) {
+        for (Map.Entry<Integer, String> entry : BUTTON_TO_URL_MAP.entrySet()) {
             Button button = view.findViewById(entry.getKey());
             button.setOnClickListener(v -> openYoutubeVideo(entry.getValue()));
         }
+    }
 
-        // Array de lista de IDs de las ImageView a las que quieres aplicar la animación
+    // Configura la animación de las ImageView
+    private void setupImageViewAnimations(View view) {
         int[] imageViewIds = {
                 R.id.imagenPBP,
                 R.id.imagenFondos,
@@ -92,25 +108,56 @@ public class RutinasFragment extends Fragment {
                 R.id.imagenExtTriceps,
                 R.id.imagenPalof
         };
-        // Configurar un listener para cada ImageView. Cuando se hace clic en una ImageView, se inicia una animación de rotación que dura 3 segundos. Después de 3 segundos, la animación se detiene
         for (int id : imageViewIds) {
             ImageView imageView = view.findViewById(id);
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate);
-                    imageView.startAnimation(animation);
-
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            imageView.clearAnimation();
-                        }
-                    }, 3000);
-                }
-            });
+            imageView.setOnClickListener(v -> startRotationAnimation(imageView));
         }
     }
+
+    // Inicia la animación de rotación en la ImageView
+    private void startRotationAnimation(ImageView imageView) {
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate);
+        imageView.startAnimation(animation);
+        new Handler().postDelayed(imageView::clearAnimation, 3000);
+    }
+
+    // Configura el temporizador de cuenta regresiva
+    private void setupCountDownTimer(View view) {
+        TextView questionText = view.findViewById(R.id.textoCronometro);
+        TextView timeRemaining = view.findViewById(R.id.timeRemaining);
+        questionText.setOnClickListener(v -> startCountDownTimer(timeRemaining));
+    }
+
+    private void startCountDownTimer(TextView timeRemaining) {
+        new CountDownTimer(60000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                long secondsRemaining = millisUntilFinished / 1000;
+                timeRemaining.setText(String.valueOf(secondsRemaining));
+            }
+
+            public void onFinish() {
+                timeRemaining.setText("0");
+                vibrateAndPlayBeep();
+            }
+        }.start();
+    }
+
+    // Vibra el teléfono tres veces y reproduce un sonido de pitido
+    private void vibrateAndPlayBeep() {
+        Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createWaveform(new long[]{0, 500, 500, 500}, -1));
+            } else {
+                vibrator.vibrate(new long[]{0, 500, 500, 500}, -1);
+            }
+        }
+
+        // Reproduce el sonido de pitido
+        MediaPlayer mediaPlayer = MediaPlayer.create(getContext(), R.raw.countdown_to_fight);
+        mediaPlayer.start();
+    }
+
 
     // Este método se utiliza para abrir un video de YouTube en el navegador. Se crea un Intent con la acción Intent.ACTION_VIEW y la URL del video de YouTube, y luego se inicia este Intent
     private void openYoutubeVideo(String url) {
