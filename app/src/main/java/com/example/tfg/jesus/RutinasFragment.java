a038758592c24d9c147c1248cdc3adb8e414187c
JavisinWayne <javisinwayne@gmail.com>
2024-05-12 16:17:12 +0200

    Actualización SplashScreen

--- app/src/main/java/com/example/tfg/jesus/RutinasFragment.java
+++ app/src/main/java/com/example/tfg/jesus/RutinasFragment.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.loginsign.R;
+import com.example.tfg.R;
 
 /**
  * A simple {@link Fragment} subclass.
