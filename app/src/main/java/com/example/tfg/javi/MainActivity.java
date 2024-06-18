12ff6ade335f729ec9deea36560bd88f8f1bbd01
JCSM17 <jcsanchezmoreno7@gmail.com>
2024-06-18 18:14:02 +0200

    18/06 Añadido nuevo perrotumbadojesus

--- app/src/main/java/com/example/tfg/javi/MainActivity.java
+++ app/src/main/java/com/example/tfg/javi/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tfg.javi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,12 +11,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.tfg.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int SPLASH_SCREEN_DELAY = 1200;
+    private static final int SPLASH_SCREEN_DELAY = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +25,18 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        // Cambiar el color de fondo de la vista raíz
+        View root = findViewById(android.R.id.content);
+        root.setBackgroundColor(ContextCompat.getColor(this, R.color.dark_navy_blue));
+
         //Agregar animaciones
         Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
         Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
 
         ImageView imagenLogo = findViewById(R.id.imagenLogo);
-        TextView textoDesde = findViewById(R.id.textoDesde);
         TextView textoJJJFIT = findViewById(R.id.textoJJJFIT);
 
 
-        textoDesde.setAnimation(animacion2);
         textoJJJFIT.setAnimation(animacion2);
         imagenLogo.setAnimation(animacion1);
 
